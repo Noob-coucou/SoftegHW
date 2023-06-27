12f53a8314e7344ef4a7192db2852c488c195aa7
philippe liu <715271039@qq.com>
2023-06-27 16:56:00 +0800

    exception specified

--- ErrorHandling/src/dividebyzero/Main.java
+++ ErrorHandling/src/dividebyzero/Main.java
@@ -7,11 +7,10 @@ public class Main {
      */
     public static void main(String[] args) {
         try {
-            System.out.println(3/0);
-        } catch (Exception e) {
-            System.out.printf("Caught runtime exception = %s\n", e.getMessage());
+            System.out.println(3 / 0);
+        } catch (ArithmeticException e) {
+            System.out.printf("Caught ArithmeticException: %s\n", e.getMessage());
         }
     }
 
 }
-
